a534d070d813bccf1ed56ffcaf4b8a51f872250f
Muniyasamy V <94435705+gvmsamy@users.noreply.github.com>
2026-03-13 10:57:19 +0530

    Enhance Projects component with demo and code links
    Updated project entries to include demo and code links.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,41 +6,47 @@ const Projects = () => {
     {
       title: "Tic Tac Toe Game",
       tech: "React JS",
-      link: "https://react-tic-tac-toe.vercel.app/"
+      demo: "https://react-tic-tac-toe.vercel.app/",
+      code: "https://github.com/gvmsamy/react-tic-tac-toe"
     },
     {
       title: "Investment Calculator",
       tech: "React JS",
-      link: "https://react-investment-calculator-sandy-eight.vercel.app/"
+      demo: "https://react-investment-calculator-sandy-eight.vercel.app/",
+      code: "https://github.com/gvmsamy/react-investment-calculator"
     },
     {
       title: "React Portfolio",
       tech: "React JS",
-     link: "https://github.com/gvmsamy/muniyasamy-portfolio"
+      demo: "https://muniyasamy-portfolio.vercel.app/",
+      code: "https://github.com/gvmsamy/muniyasamy-portfolio"
     }
   ];
 
   return (
-    <section  id="projects" className="projects">
+    <section id="projects" className="projects">
       <h2>Projects</h2>
 
       <div className="project-container">
         {projects.map((project, index) => (
-          <a
-            key={index}
-            href={project.link}
-            target="_blank"
-            rel="noreferrer"
-            className="project-card"
-          >
+          <div key={index} className="project-card">
             <h3>{project.title}</h3>
             <p>{project.tech}</p>
-          </a>
+
+            <div className="project-links">
+              <a href={project.demo} target="_blank" rel="noreferrer">
+                Demo
+              </a>
+
+              <a href={project.code} target="_blank" rel="noreferrer">
+                Code
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </section>
   );
 };
-
 
 export default Projects;
